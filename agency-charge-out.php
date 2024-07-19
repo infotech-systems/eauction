da7b426649247fd23bc57fbcc7c930c7090eb2d0
Amitabha-Barat <67781504+Amitabha-Barat@users.noreply.github.com>
2024-07-19 17:15:41 +0530

    agency-190724

--- agency-charge-out.php
+++ agency-charge-out.php
@@ -2,7 +2,7 @@
 $submit = isset($_POST['submit']) ? $_POST['submit'] : '';
 if($submit=="Excel")
 {
-    $database='Committee-Approval';
+    $database='Agency-Charge-Approval';
     header("Content-type: text/plan");  
     header("Content-Disposition: attachment; filename=" . $database . ".xls");   
 }
@@ -75,7 +75,7 @@ if($tk>0)
         <head>
             <meta charset="utf-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <title>Committee Approval Report</title>
+            <title>Agency Charge Report</title>
             <?php
             if($submit=="Submit")
             {
@@ -134,43 +134,26 @@ if($tk>0)
                                     {
                                         ?>
                                         <tr>
-                                            <td rowspan="3" align="center"><img src="<?php echo $full_url; ?>/images/logo.png" width="35" hight="45"/></td>
-                                            <td  colspan="<?php if($ses_user_type!='B'){ ?> 11 <?php } else { ?>9<?php } ?>" align="center" style=" border-bottom:none !important;">&nbsp;</td> 
+                                            <td rowspan="2" align="left"><img src="<?php echo $full_url; ?>/images/logo.png" width="35" hight="45"/></td>
+                                           
                                         </tr>
                                         <?php
                                     }
                                     ?>
                                         <tr>
-                                            <td colspan="<?php if($ses_user_type!='B'){ ?> 12 <?php } else { ?>10<?php } ?>" align="center"  style=" border-top:none !important;border-bottom:none !important;"><B><?php echo "<b>$ses_orgn_nm</b> <br /> $ses_orgn_addr";?></B></td>
+                                            <td colspan="3" align="center"  style=" border-top:none !important;"><B><?php echo "<b>$ses_orgn_nm</b> <br /> $ses_orgn_addr";?></B></td>
                                         </tr>
                                         <tr> 
-                                            <td colspan="6"><B>Offer Sheet No: <?php echo $offersheet;?></B></td>
-                                            <td colspan="<?php if($ses_user_type!='B'){ ?> 5 <?php } else { ?>4<?php } ?>" align="right"  style=" border-top:none !important;"><B>Print Date : <?php echo $print_date; ?> Time : <?php echo $print_dt[1]; ?></B></td>
+                                            <td ><B>Offer Sheet No: <?php echo $offersheet;?></B></td>
+                                            <td align="right" colspan="2" style=" border-top:none !important;"><B>Print Date : <?php echo $print_date; ?> Time : <?php echo $print_dt[1]; ?></B></td>
                                         </tr>
                                     </thead> 
-                                    <tr>
-                                        <td><B>Sl.</B></td>
-                                    <!--    <td><B>Lot No</B></td>-->
-                                        <td><B>Garden</B></td>
-                                        <td><B>Grade</B></td>
-                                        <td><B>Invoice</B></td>
-                                        <td><B>Pkgs</B></td>
-                                        <td><B>Weight (Kg)</B></td>
-                                        <td><B>Valuation</B></td>
-                                        <td><B>HBP</B></td>
-                                        <td><B>Bill Value</B></td>
-                                        <td><B>Billing Name</B></td>
-                                        <td><B>Bid Price 1</B></td>
-                                        <td><B>Bid Price 2</B></td>
-                                        <td><B>Bid Price 3</B></td>
-                                        <td><B>Bid Price 4</B></td>
-                                    </tr>
                                     <?php
                                     $sl=0;
                                     $Tbill_value=0;
                                     $Tkg=0;
                                     $sqle= "select f.acd_id,f.auc_id,f.lot_no,f.garden_nm,f.grade,f.pkgs,f.net,f.invoice_no,f.msp,f.valu_kg ";
-                                    $sqle.=",f.base_price,f.bid_price,b.billing_nm ";
+                                    $sqle.=",f.base_price,f.bid_price,b.billing_nm,b.name,b.bidder_type ";
                                     $sqle.="from final_auction_dtl f, bidder_mas b ";
                                     $sqle.="where  offer_srl=:offersheet and f.bidder_id=b.bidder_id and f.all_app='Y' ";
                                     $sth = $conn->prepare($sqle);
@@ -195,99 +178,34 @@ if($tk>0)
                                         $bid_price=$value['bid_price'];
                                         $base_price=$value['base_price'];
                                         $billing_nm=$value['billing_nm'];
+                                        $agency_name=$value['name'];
+                                        $bidder_type=$value['bidder_type'];
 
-                                        $sql=" select bid_price from fin_auc_bid_dtl ";
-                                        $sql.=" where bid_price < :bid_price ";
-                                        $sql.=" and auc_id=:auc_id and acd_id=:acd_id ";
-                                        $sql.=" order by bid_price desc limit 1 ";
-                                        $sth = $conn->prepare($sql);
-                                        $sth->bindParam(':bid_price', $bid_price);                                        
-                                        $sth->bindParam(':auc_id', $auc_id);                                        
-                                        $sth->bindParam(':acd_id', $acd_id);                                        
-                                        $sth->execute();
-                                        $ss=$sth->setFetchMode(PDO::FETCH_ASSOC);
-                                        $row2 = $sth->fetch();
-                                        $bid_price1=null;
-                                        if($row2)
-                                        {
-                                            $bid_price1=$row2['bid_price'];
-                                        }
-                                        $sql=" select bid_price as bid_price2 from fin_auc_bid_dtl ";
-                                        $sql.=" where bid_price < :bid_price1 ";
-                                        $sql.=" and auc_id=:auc_id and acd_id=:acd_id ";
-                                        $sql.=" order by bid_price desc limit 1 ";
-                                        $sth = $conn->prepare($sql);
-                                        $sth->bindParam(':bid_price1', $bid_price1);                                        
-                                        $sth->bindParam(':auc_id', $auc_id);                                        
-                                        $sth->bindParam(':acd_id', $acd_id);                                        
-                                        $sth->execute();
-                                        $ss=$sth->setFetchMode(PDO::FETCH_ASSOC);
-                                        $row2 = $sth->fetch();
-                                        $bid_price2=null;
-                                        if($row2)
-                                        {
-                                            $bid_price2=$row2['bid_price2'];
-                                        }
-                                        $sql=" select bid_price as bid_price3 from fin_auc_bid_dtl ";
-                                        $sql.=" where bid_price < :bid_price2 ";
-                                        $sql.=" and auc_id=:auc_id and acd_id=:acd_id ";
-                                        $sql.=" order by bid_price desc limit 1 ";
-                                        $sth = $conn->prepare($sql);
-                                        $sth->bindParam(':bid_price2', $bid_price2);                                        
-                                        $sth->bindParam(':auc_id', $auc_id);                                        
-                                        $sth->bindParam(':acd_id', $acd_id);                                        
-                                        $sth->execute();
-                                        $ss=$sth->setFetchMode(PDO::FETCH_ASSOC);
-                                        $row2 = $sth->fetch();
-                                        $bid_price3=null;
-                                        if($row2)
-                                        {
-                                            $bid_price3=$row2['bid_price3'];
-                                        }
                                         $bill_value=0;
                                         $bill_value=$net*$bid_price;
                                         $Tbill_value=$Tbill_value+$bill_value;
                                         $Tkg=$Tkg+$net;
-                                        ?>
-                                        <tr>
-                                            <td align="center"><?php echo $sl;?></td>
-                                        <!--    <td align="center"><?php echo $lot_no;?></td>-->
-                                            <td align="center"><?php echo $garden_nm;?></td>
-                                            <td align="center"><B><?php echo $grade;?></b></td>
-                                            <td align="center"><?php echo $invoice_no;?></td>
-                                            <td align="center"><?php echo $pkgs;?></td>
-                                            <td align="center"><?php echo $net;?></td>
-                                            <td align="center"><?php echo $valu_kg;?></td>
-                                            <td align="center"><?php echo $bid_price;?></td>
-                                            <td align="right"><?php echo number_format($bill_value,2);?></td>
-                                            <td align="center"><?php echo $billing_nm;?></td>
-                                            <td align="center"><?php echo $bid_price;?></td>
-                                            <td align="center"><?php echo $bid_price1;?></td>
-                                            <td align="center"><?php echo $bid_price2;?></td>
-                                            <td align="center"><?php echo $bid_price3;?></td>                                            
-                                        <tr>
-                                        <?php            
+                                        $agency_chrg=0;
+                                        if($bidder_type=="A")
+                                        $agency_chrg=($Tbill_value*1)/100;       
                                     }
                                     ?>
-                                    <tr>
-                                        <td colspan="5" align="right"><B>Total</B></td>
-                                        <td align="center"><B><?php echo $Tkg;?></B></td>
-                                        <td colspan="2">&nbsp;</td>
-                                        <td align="right"><B><?php echo number_format($Tbill_value,2);?></B></td>
-                                        <td colspan="5">&nbsp;</td>
-                                    </tr>
-
+                                    <tr><td><B>Buyer's Name</B></td><td><B><?php echo $billing_nm;?></B></td></tr>
+                                    <tr><td><B>Agent's Name</B></td><td><B><?php echo $agency_name;?></B></td></tr>
+                                    <tr><td><B>Total Qty Sold</B></td><td><B><?php echo $Tkg;?> Kg.</B></td></tr>
+                                    <tr><td><B>No of Invoice</B></td><td><B><?php echo $sl;?> </B></td></tr>
+                                    <tr><td><B>Total Invoice Value</B></td><td><B><?php echo number_format($Tbill_value,2);?></B> </td></tr>
+                                    <tr><td><B>Agency Charge @ 1% of Bill Value</B></td><td><B><?php echo number_format($agency_chrg,2);?></B> </td></tr>                                       
                                     </table>
                                     <table style="margin-top:30px; width:100%;">
                                         <tr>
                                             <?php
                                             $sl=0;
                                             $sqle= "select u.design_nm,u.signature";
-                                            $sqle.=" from bid_app_dtl b,user_mas u ";
-                                            $sqle.="where b.uid=u.uid and  auc_id=:auc_id group by b.uid order by b.seq_id";
-
+                                            $sqle.=" from user_mas u ";
+                                            $sqle.="where u.uid=:ses_uid ";
                                             $sth = $conn->prepare($sqle);
-                                            $sth->bindParam(':auc_id', $auc_id);
+                                            $sth->bindParam(':ses_uid', $ses_uid);
                                             $sth->execute();
                                             $ss=$sth->setFetchMode(PDO::FETCH_ASSOC);
                                             $row = $sth->fetchAll();
---
+++ agency-charge.php
@@ -0,0 +1,56 @@
+<?php 
+header("X-XSS-Protection: 1;mode = block");
+include('./header.php'); 
+error_reporting(1);
+?>
+<div id="preloder">
+  <div class="loader"></div>
+</div>
+<form name="form1"  id="fileUploadForm"  method="post" class="form-horizontal" enctype="multipart/form-data" autocomplete="off" action="agency-charge-out.php" target="_blank">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-success">
+                <div class="box-header  with-border">
+                    <h3 class="box-title">Search Information</h3>  
+                </div>
+                    <div class="box-body">
+                        <div class="col-md-6">
+                            <div class="form-group  has-feedback">
+                                <label for="place" class="col-sm-4">Offsheet No</label>
+                                <div class="col-sm-8">
+                                    <select name="offersheet" id="offersheet" class="form-control select2" required>
+                                        <option value=""></option>
+                                        <?php
+                                        $sqle= "select offer_srl ";
+                                        $sqle.="from final_auction_dtl group by offer_srl order by offer_srl";
+                                        $sth = $conn->prepare($sqle);
+                                        $sth->execute();
+                                        $ss=$sth->setFetchMode(PDO::FETCH_ASSOC);
+                                        $row = $sth->fetchAll();
+                                        foreach ($row as $key => $value) 
+                                        {
+                                            $offer_srl=$value['offer_srl'];
+                                            $place=$value['place'];
+                                            ?>
+                                            <option value="<?php echo $offer_srl; ?>"><?php echo $offer_srl; ?></option>
+                                            <?php
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-footer">
+                        <input type="submit" name="submit" id="submit" class="btn btn-danger" value="Excel" tabindex="13">
+                        <input type="submit" name="submit" id="submit" class="btn btn-success pull-right" value="Submit" tabindex="13">
+                    </div>
+            </div>
+        </div>
+    </div>
+</form>
+<?php 
+
+include('./footer.php'); ?>
+<script src="<?php echo $full_url; ?>/customjs/excel-upload.js?v=<?php echo date('YmdHis'); ?>"></script>
+
